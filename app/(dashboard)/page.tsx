346cdf2761b5eb061ad94e24ac7cee449ef21b4c
Prithivi <prithivi@users.noreply.github.com>
2026-04-09 17:07:54 +0545

    feat: add Separator component for improved UI layout in dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 import CardStatsWrapper, { StatsCards } from "@/components/card-stats-wrapper";
+import { Separator } from "@/components/ui/separator";
 import { Suspense } from "react";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
       <Suspense fallback={<StatsCards loading={true} />}>
         <CardStatsWrapper />
       </Suspense>
+      <Separator className="my-6" />
+      <h2 className="text-4xl font-bold col-span-2">Your Forms</h2>
+      <Separator className="my-6" />
     </div>
   );
 }
